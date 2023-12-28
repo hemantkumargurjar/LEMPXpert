c7530ec7000d4e479cf9ef4056d3e90a7ccbc6a0
Hemant Kumar <85330120+hemantkumargurjar@users.noreply.github.com>
2023-12-29 01:06:50 +0530

    init

---
+++ lemp_manager.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# ASCII art logo using figlet
+logo_text=$(figlet "LEMPXpert")
+
+welcome_message="Welcome to LEMPXpert - Your LEMP Server Manager"
+
+display_menu() {
+    clear
+    echo "$logo_text"
+    echo "$welcome_message"
+    echo "Select an option:"
+    echo "1. Install LEMPXpert"
+    echo "2. Test Server"
+    echo "3. Exit"
+}
+
+install_lempxpert() {
+    # Implement installation logic here
+    echo "Installing LEMPXpert..."
+    # Add your installation steps here
+}
+
+test_server() {
+    # Implement server testing logic here
+    echo "Testing Server..."
+    # Add your server testing steps here
+}
+
+while true; do
+    display_menu
+    read -p "Enter your choice (1/2/3): " choice
+
+    case "$choice" in
+        1)
+            install_lempxpert
+            ;;
+        2)
+            test_server
+            ;;
+        3)
+            echo "Exiting LEMPXpert. Goodbye!"
+            exit 0
+            ;;
+        *)
+            echo "Invalid choice. Please select a valid option (1/2/3)."
+            ;;
+    esac
+done
