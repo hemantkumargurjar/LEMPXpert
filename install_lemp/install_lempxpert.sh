f1c0c9fe54d9460a5ff071be7cbd457ce4e79fbe
Hemant Kumar <85330120+hemantkumargurjar@users.noreply.github.com>
2023-12-29 08:15:35 +0530

    Update install_lempxpert.sh

--- install_lemp/install_lempxpert.sh
+++ install_lemp/install_lempxpert.sh
@@ -275,7 +275,7 @@ EOF
 
 # Main script
 read -p "Enter 'stable' or 'mainline' to choose the Nginx version: " nginx_version_branch
-$root_dir = "/home/lempxpert.server/public"  # Change this to your desired root directory path
+root_dir="/home/lempxpert.server/public"  
 mkdir -p "$root_dir"
 # Validate the root directory path
 if [ -z "$root_dir" ] || [ ! -d "$root_dir" ]; then
